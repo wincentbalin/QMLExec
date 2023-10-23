b1e69fae8fa44187c76166714f15c03714899641
Wincent Balin <wincent.balin@gmail.com>
2023-10-24 01:45:47 +0200

    Added app path to import paths

--- main.cpp
+++ main.cpp
@@ -11,12 +11,16 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(QDir(QGuiApplication::applicationDirPath()).filePath(QStringLiteral("main.qml")));
+    const QString appDir = QGuiApplication::applicationDirPath();
+    const QString mainFile = QDir(appDir).filePath(QStringLiteral("main.qml"));
+    const QUrl url(mainFile);
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
             QCoreApplication::exit(-1);
     }, Qt::QueuedConnection);
+    engine.addImportPath(appDir);
+    engine.addPluginPath(appDir);
     engine.load(url);
 
     return app.exec();
