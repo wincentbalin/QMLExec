c8f1767c34c99798e11916c7a32c647871f97e01
Wincent Balin <wincent.balin@gmail.com>
2023-10-25 01:11:12 +0200

    Added Qt.labs QML plugins

--- main.cpp
+++ main.cpp
@@ -2,6 +2,22 @@
 #include <QQmlApplicationEngine>
 #include <QDir>
 
+#ifdef STATIC_LIBRARY
+#include <QtPlugin>
+Q_IMPORT_PLUGIN(QtLabsAnimationPlugin)
+Q_IMPORT_PLUGIN(QtLabsCalendarPlugin)
+Q_IMPORT_PLUGIN(QmlFolderListModelPlugin)
+Q_IMPORT_PLUGIN(BodymovinPlugin)
+Q_IMPORT_PLUGIN(QtLabsPlatformPlugin)
+Q_IMPORT_PLUGIN(QtQmlLabsModelsPlugin)
+Q_IMPORT_PLUGIN(QmlSettingsPlugin)
+Q_IMPORT_PLUGIN(QtQuickSharedImagePlugin)
+Q_IMPORT_PLUGIN(QmlWavefrontMeshPlugin)
+#ifdef Q_OS_WIN
+Q_IMPORT_PLUGIN(QWinExtrasQmlPlugin)
+#endif
+#endif
+
 
 int main(int argc, char *argv[])
 {
